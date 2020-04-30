71bb7ec38d82a389c62354ea0924121bbe2b3b0c
Leonardo Burati <leonardoburati@hotmail.com>
2020-04-30 18:15:47 -0300

    Test da aplicação

---
+++ backend/tests/integration/ong.spec.js
@@ -0,0 +1,26 @@
+const request = require('supertest');
+const app = require('../../src/app');
+const connection = require('../../src/database/connection');
+
+describe('ONG', () => {
+  beforeEach(async () => {
+    await connection.migrate.rollback();
+    await connection.migrate.latest();
+  });
+
+  afterAll(async () => {
+    await connection.destroy();
+  });
+
+  it('should be able to create a new ONG', async () => {
+    const response = await request(app).post('/ongs').send({
+      name: 'SOS Animal 2',
+      email: 'contato@teste.com',
+      whatsapp: '48993945643',
+      city: 'Criciuma',
+      uf: 'SC',
+    });
+    expect(response.body).toHaveProperty('id');
+    expect(response.body.id).toHaveLength(8);
+  });
+});
